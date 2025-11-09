35c0df0b7256e520b90e2204966b1e9fce6fdb39
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2025-11-09 23:55:37 +0200

    second resolve

--- 2-arguments.js
+++ 2-arguments.js
@@ -4,8 +4,8 @@ if(args.length === 0){
     console.log("No argument")
 }
 else if(args.length === 1){
-    console.log("argument found")
+    console.log("Argument found")
 }
 else{
-    console.log("arguments found")
+    console.log("Arguments found")
 }
